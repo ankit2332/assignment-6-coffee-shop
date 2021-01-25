0946ccf3d82767746ae74fe76b7f92429502d753
HP <HP@DESKTOP-B0OK1FL>
2021-01-25 11:15:20 +0700

    Add project files.

---
+++ assignment 6 coffee shop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace assignment_6_coffee_shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            drink[] drinks = new drink[10];
+
+            drinks[0] = new drink("coffee", 50);
+            drinks[1]= new drink("tea", 45);
+            drinks[2] = new drink("juice", 30);
+            drinks[3] = new drink("coffee", 50);
+
+            int count = 0;
+            for(int i = 0; i < drinks.Length; i++)
+            {
+                if(drinks[i] != null)
+                {
+                    count = count + 1;
+                }
+            }
+        }
+    }
+}
---
+++ assignment 6 coffee shop/drink.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assignment_6_coffee_shop
+{
+    class drink
+    {
+        private string name;
+        private int price;
+
+        public drink(string name, int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public string getname()
+        {
+            return name;
+        }
+
+        public int getprice()
+        {
+            return price;
+        }
+    }
+
+}
